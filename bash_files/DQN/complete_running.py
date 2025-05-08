28d236ecfc2676b28bbca41807e342f40c0a2c3b
Mohamed Ayman <mohamedayman15069@aucegypt.edu>
2025-05-08 06:20:29 -0700

    updated the loading scripts

--- bash_files/DQN/complete_running.py
+++ bash_files/DQN/complete_running.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import uuid
+import json
 
 def find_checkpoints(base_folder):
     folder_paths = []
@@ -38,15 +39,39 @@ def find_checkpoints(base_folder):
                         with open(csv_path, 'r') as f:
                             lines = f.readlines()
                             if lines:
-                                last_step = int(lines[-1].strip().split(",")[-2])+1
-                                steps.append(last_step)
-                                # print(f"Last step in {csv_path}: {last_step}")
-                                folder_paths.append(root)
+                                checkpoint_number = int(os.path.basename(max_checkpoint).split("_")[0])
+                                filtered_lines = [
+                                    line for line in lines
+                                    if line.strip().split(",")[1].isdigit() and int(line.strip().split(",")[1]) <= checkpoint_number + 5000
+                                ]
+                                if filtered_lines:
+                                    # Overwrite the episodic_rewards.csv file with the filtered lines
+                                    with open(csv_path, 'w') as f:
+                                        f.writelines(filtered_lines)
+                                    
+                                    # Find the maximum step value from the filtered lines
+                                    last_step = max(int(line.strip().split(",")[1]) for line in filtered_lines) + 1
+                                    steps.append(last_step)
+                                else:
+                                    steps.append(checkpoint_number)
+                                folder_paths.append(os.path.join(root, dir))
                     else: 
                         steps.append(0)
-                        folder_paths.append(root)
+                        folder_paths.append(os.path.join(root, dir))
 
     return checkpoints, steps, folder_paths
+
+
+def get_args(checkpoint_path):
+
+        args_file = os.path.join(os.path.dirname(checkpoint_path), "args.txt")
+        if not os.path.exists(args_file):
+            raise FileNotFoundError(f"args.txt not found in {os.path.dirname(checkpoint_path)}")
+
+        with open(args_file, "r") as f:
+            args = json.load(f)
+
+        return args
 
 def modify_and_run_script(base_folder_sh,base_command, checkpoints_path):
     checkpoints, steps, folder_paths = find_checkpoints(checkpoints_path)
@@ -56,6 +81,10 @@ def modify_and_run_script(base_folder_sh,base_command, checkpoints_path):
         
         # Prepare the command with the checkpoint path
         new_command = f"{base_command} --load {checkpoint} --step-offset {steps[i]} --outdir {folder_paths[i]}"
+        args = get_args(checkpoint)
+
+        args_string = " ".join([f"--{key} {value}" for key, value in args.items() if key != "step_offset"])
+        new_command += f" {args_string}"
         with open(base_folder_sh, 'r') as file:
             lines = file.readlines()
         
@@ -65,7 +94,7 @@ def modify_and_run_script(base_folder_sh,base_command, checkpoints_path):
             file.write(new_command + "\n")
         
         # # Execute the modified script using sbatch
-        os.system(f'sbatch {new_file}')
+        # os.system(f'sbatch {new_file}')
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
